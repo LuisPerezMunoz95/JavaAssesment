560156e87e58e42ec271f1b7c16f9a9ea3dd7e9d
PCM28 <104649488+PCM28@users.noreply.github.com>
2022-12-05 10:56:59 +0100

    First Part

---
+++ src/main/java/com/example/JavaAzureAssesment/JavaAzureAssesmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaAzureAssesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAzureAssesmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAzureAssesmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JavaAzureAssesment/JavaAzureAssesmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaAzureAssesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAzureAssesmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
